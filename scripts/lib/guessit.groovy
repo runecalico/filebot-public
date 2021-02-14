52fd16b71def2a1c53f3a3fdf11c434bda53acf2
Jonathan Kaufman <webreg@tir-nan-og.com>
2021-02-14 08:17:47 -0600

    lib - guessit - v1.0.0

---
+++ scripts/lib/guessit.groovy
@@ -0,0 +1,13 @@
+package lib
+//--- VERSION 1.0.0
+
+import org.apache.commons.io.FilenameUtils
+
+def executeGuessit(String fileName, String pathToGuessitExecutable){
+  String sanitizedGuessitPath = FilenameUtils.separatorsToSystem(pathToGuessitExecutable)
+  def guessitCommand = "sanitizedGuessitPath" + " -L EN -s -j \"${fileName}\""
+  def proc = guessitCommand.execute()
+  def outputStream = new StringBuffer()
+  proc.waitForProcessOutput(outputStream, System.err)
+  return outputStream.toString().trim()
+}
